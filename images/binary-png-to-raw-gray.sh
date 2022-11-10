7da8eafcdbb972dfd6cb13d198235e6bdf536e6d
Brian Ballantine <brianballantine@gmail.com>
2022-11-09 23:07:15 -0500

    updated images, stm32411 black pill has arrived

--- images/binary-png-to-raw-gray.sh
+++ images/binary-png-to-raw-gray.sh
@@ -1,6 +1,6 @@
 COUNTER=1
-for file in ./*.png
+for file in ../../image-workspace/128x64/png/*.png
 do
-  convert "$file" -depth 1 "$COUNTER.gray"
+  convert "$file" -depth 1 "128x64/`printf %03d $COUNTER`.gray"
   COUNTER=$[$COUNTER +1]
 done
