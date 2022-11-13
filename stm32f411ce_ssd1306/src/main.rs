85410dd4327e9ce6f08c399eebab36a0fec89757
Brian Ballantine <brianballantine@gmail.com>
2022-11-13 08:01:23 -0500

    Finally wrangled a function out of the thing

--- stm32f411ce_ssd1306/src/main.rs
+++ stm32f411ce_ssd1306/src/main.rs
@@ -3,7 +3,7 @@
 
 use panic_halt as _;
 
-use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306, size::DisplaySize128x64};
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306, mode::BufferedGraphicsMode, size::DisplaySize128x64};
 use stm32f4xx_hal as hal;
 
 use core::{fmt::Write};
@@ -16,12 +16,27 @@ use embedded_graphics::{
     prelude::*,
     text::{Baseline, Text},
 };
-use hal::pac;
-use hal::rcc;
+use hal::{i2c::I2c, pac};
 use hal::prelude::*;
 use heapless::String;
 
 mod images;
+
+// https://stackoverflow.com/questions/58075821/rust-embedded-binary-size
+#[inline]
+fn my_draw<T: stm32f4xx_hal::i2c::Instance, U>(
+    img: usize,
+    target: &mut Ssd1306<I2CInterface<I2c<T, U>>,
+                            DisplaySize128x64,
+                            BufferedGraphicsMode<DisplaySize128x64>
+                            >) -> ()
+{
+    target.clear();
+    let raw2: ImageRaw<BinaryColor> =ImageRaw::new(images::IMAGES[img], 128);
+    let im = Image::new(&raw2, Point::new(0,0));
+    im.draw(target).unwrap();
+    target.flush().unwrap();
+}
 
 #[entry]
 fn main() -> ! {
@@ -112,21 +127,6 @@ fn main() -> ! {
 
     display3.init().unwrap();
 
-    fn rando_g(tim3: pac::TIM3, tim4: pac::TIM4, clocks: rcc::Clocks) -> u32 {
-        let mut counter_a = tim3.counter_ms(&clocks);
-        let mut counter_b = tim4.counter_us(&clocks);
-        counter_a.start(20000.millis()).unwrap();
-        counter_b.start(6501.micros()).unwrap();
-        nb::block!(counter_a.wait()).unwrap();
-        let ret_val = counter_b.now().ticks();
-        counter_a.cancel().unwrap();
-        counter_b.cancel().unwrap();
-        ret_val
-    }
-
-    let rand_n = rando_g(dp.TIM3, dp.TIM4, clocks);
-    //let rand_n = 0;
-
     // Configure App Counter
 
     let mut app_counter = dp.TIM2.counter_ms(&clocks);
@@ -134,8 +134,10 @@ fn main() -> ! {
     let dur: Duration<u32, 1, 1000> = 15000.millis();
     app_counter.start(dur).unwrap();
 
-    let mut curr_img2 = 0;
-    let mut curr_img3 = 150;
+    let mut im1 = 0;
+    let mut im2 = 150;
+    my_draw(im1, &mut display2);
+    my_draw(im2, &mut display3);
     loop {
 
         display.clear();
@@ -152,31 +154,20 @@ fn main() -> ! {
         let mut sums_msg: String<20> = String::from("");
         if app_counter.wait().is_ok() {
 
-            write!(sums_msg,"Cool {}... ok {}", curr_img2, rand_n).unwrap();
+            im1 = im1 + 1;
+            im2 = im2 + 1;
 
-            // display2
-            if curr_img2 == images::IMAGES.len() {curr_img2 = 0};
-            let im2 = images::IMAGES[curr_img2];
-            display2.clear();
-            let raw2: ImageRaw<BinaryColor> =ImageRaw::new(im2, 128);
-            let im2 = Image::new(&raw2, Point::new(0, 0));
-            im2.draw(&mut display2).unwrap();
-            display2.flush().unwrap();
+            if im1 == images::IMAGES.len() {im1 = 0};
+            if im2 == images::IMAGES.len() {im2 = 0};
 
-            // display 3
-            if curr_img3 == images::IMAGES.len() {curr_img3 = 0};
-            let im3 = images::IMAGES[curr_img3];
-            display3.clear();
-            let raw3: ImageRaw<BinaryColor> =ImageRaw::new(im3, 128);
-            let im3 = Image::new(&raw3, Point::new(0, 0));
-            im3.draw(&mut display3).unwrap();
-            display3.flush().unwrap();
+            write!(sums_msg,"flipping {}, {}", im1, im2).unwrap();
 
-            curr_img2 = curr_img2 + 1;
-            curr_img3 = curr_img3 + 1;
+            my_draw(im1, &mut display2);
+            my_draw(im2, &mut display3);
+
         }
         else {
-            write!(sums_msg,"Just_ {}... ok {}", curr_img2, rand_n).unwrap();
+            write!(sums_msg,"showing {}, {}", im1, im2).unwrap();
         }
 
         Text::with_baseline(&sums_msg, Point::new(0, 48), text_style, Baseline::Top)
